11d5ba5ef5d4aab20eaacc3fe3f4ff3c97e70660
Giteq <patrykkotlowski@gmail.com>
2018-11-09 14:55:18 +0100

    Changed path of file with measurment ti universal one, and changed Accelerometer used in main to working one.

--- RaspberryPi_drivers/src/main.cpp
+++ RaspberryPi_drivers/src/main.cpp
@@ -8,12 +8,12 @@
 #define RED_LED_OFF "python3 /home/pi/Documents/Git/PolesDetection/RaspberryPi_drivers/src/green_led_off.py"
 #define RED_LED_ON  "python3 /home/pi/Documents/Git/PolesDetection/RaspberryPi_drivers/src/green_led_on.py"
 
-std::ofstream file("/home/pi/Documents/Git/PolesDetection/RaspberryPi_drivers/bin/measurment.txt",std::fstream::out | std::fstream::app);
+std::ofstream file("../bin/measurment.txt",std::fstream::out | std::fstream::app);
 
 int main(void)
 {	
 	int16_t measure_data[3u];
-	Accelerometer acc(1);
+	Accelerometer acc(0);
 	std::chrono::duration<double> time;
 	
 	auto start = std::chrono::high_resolution_clock::now();
